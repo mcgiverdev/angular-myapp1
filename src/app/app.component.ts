0843312ce90d8ad6d17ec98c7907d600be9235da
McGiver <bavila@canvia.com>
2022-10-17 11:44:35 -0500

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app-1';
+  person = {
+    name: "",
+    lastName: "",
+    age: 0
+  }
+  onRegister=() => {
+    console.log("onRegister", this.person)
+  }
 }
